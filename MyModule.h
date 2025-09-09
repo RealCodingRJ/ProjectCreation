2f48fe71c006ca7f0ed6025122ca2afade927436
Priates of Zimsey <realcoder2023@gmail.com>
2025-09-09 12:11:12 -0400

    added code for v1

---
+++ MyModule.h
@@ -0,0 +1,7 @@
+﻿#include <string>
+#include <Windows.h>
+
+void createWindowsCommand(const std::string& command) {
+    ShellExecuteA(nullptr, "open", command.c_str(),
+        nullptr, nullptr, SW_SHOWDEFAULT );
+}
---
+++ main.cpp
@@ -0,0 +1,79 @@
+﻿#include <cstring>
+#include <iostream>
+#include <thread>
+#include "MyModule.h"
+#include <Windows.h>
+
+std::string (*ANSWER)(const std::string*);
+
+std::string isAnswer(const std::string *option) {
+    return *option;
+}
+
+void printMessage() {
+    std::cout << "Function Rand Fine.." << std::endl;
+}
+
+void COMPLETED_FUNCTION() {
+    std::cout << "[COMPLETED]" << std::endl;
+}
+
+void printMessageIntro(const std::string& INTROMessage) {
+    std::cout << INTROMessage << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+
+    std::string answer;
+    if (strcmp(argv[0], "-r") == 0) {
+        createWindowsCommand("npx create-nano-react-app myapp");
+        COMPLETED_FUNCTION();
+        exit(0);
+    }
+
+    Sleep(3000);
+    printMessageIntro("Enter Option: ");
+
+    ANSWER = &isAnswer;
+    std::cin >> answer;
+
+    std::thread th(printMessage);
+
+    th.join();
+
+    if (strcmp(answer.c_str(), "React") == 0) {
+        std::cout << ANSWER(&answer) << std::endl;
+        createWindowsCommand("npx create-nano-react-app myapp");
+        COMPLETED_FUNCTION();
+
+        exit(0);
+    }
+
+    if (strcmp(answer.c_str(), "Ts") == 0) {
+        std::cout << ANSWER(&answer) << std::endl;
+        createWindowsCommand("npx nano-react-app --ts");
+        COMPLETED_FUNCTION();
+
+        exit(0);
+    }
+
+    if (strcmp(answer.c_str(), "Next") == 0) {
+
+        std::cout << ANSWER(&answer) << std::endl;
+        createWindowsCommand( "npx create-next-app");
+        COMPLETED_FUNCTION();
+
+        exit(0);
+    }
+
+    if (strcmp(answer.c_str(), "Vite") == 0) {
+
+        std::cout << ANSWER(&answer) << std::endl;
+        createWindowsCommand( "npm create vite");
+        COMPLETED_FUNCTION();
+
+        exit(0);
+    }
+
+    return 0;
+}
