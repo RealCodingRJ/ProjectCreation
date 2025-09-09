ab1061dcadbc568823f82f8cfb48013027539209
Priates of Zimsey <realcoder2023@gmail.com>
2025-09-09 13:22:59 -0400

    added some namespaces to code be read easily

--- main.cpp
+++ main.cpp
@@ -4,35 +4,43 @@
 #include "MyModule.h"
 #include <Windows.h>
 
+namespace Type {
+
+    void printMessage() {
+        std::cout << "Function Rand Fine.." << std::endl;
+    }
+
+    void COMPLETED_FUNCTION() {
+        std::cout << "[COMPLETED]" << std::endl;
+    }
+
+    void printMessageIntro(const std::string& INTROMessage) {
+        std::cout << INTROMessage << std::endl;
+    }
+
+
+}
+
+void printMessage();
+
 std::string (*ANSWER)(const std::string*);
+
 
 std::string isAnswer(const std::string *option) {
     return *option;
-}
-
-void printMessage() {
-    std::cout << "Function Rand Fine.." << std::endl;
-}
-
-void COMPLETED_FUNCTION() {
-    std::cout << "[COMPLETED]" << std::endl;
-}
-
-void printMessageIntro(const std::string& INTROMessage) {
-    std::cout << INTROMessage << std::endl;
 }
 
 int main(int argc, char* argv[]) {
 
     std::string answer;
     if (strcmp(argv[0], "-r") == 0) {
-        createWindowsCommand("npx create-nano-react-app myapp");
-        COMPLETED_FUNCTION();
+        SystemCommands::createWindowsCommand("npx create-nano-react-app myapp");
+        Type::COMPLETED_FUNCTION();
         exit(0);
     }
 
     Sleep(3000);
-    printMessageIntro("Enter Option: ");
+    Type::printMessageIntro("Enter Option: ");
 
     ANSWER = &isAnswer;
     std::cin >> answer;
@@ -43,16 +51,16 @@ int main(int argc, char* argv[]) {
 
     if (strcmp(answer.c_str(), "React") == 0) {
         std::cout << ANSWER(&answer) << std::endl;
-        createWindowsCommand("npx create-nano-react-app myapp");
-        COMPLETED_FUNCTION();
+        SystemCommands::createWindowsCommand("npx create-nano-react-app myapp");
+        Type::COMPLETED_FUNCTION();
 
         exit(0);
     }
 
     if (strcmp(answer.c_str(), "Ts") == 0) {
         std::cout << ANSWER(&answer) << std::endl;
-        createWindowsCommand("npx nano-react-app --ts");
-        COMPLETED_FUNCTION();
+        SystemCommands::createWindowsCommand("npx nano-react-app --ts");
+        Type::COMPLETED_FUNCTION();
 
         exit(0);
     }
@@ -60,8 +68,8 @@ int main(int argc, char* argv[]) {
     if (strcmp(answer.c_str(), "Next") == 0) {
 
         std::cout << ANSWER(&answer) << std::endl;
-        createWindowsCommand( "npx create-next-app");
-        COMPLETED_FUNCTION();
+        SystemCommands::createWindowsCommand( "npx create-next-app");
+        Type::COMPLETED_FUNCTION();
 
         exit(0);
     }
@@ -69,8 +77,8 @@ int main(int argc, char* argv[]) {
     if (strcmp(answer.c_str(), "Vite") == 0) {
 
         std::cout << ANSWER(&answer) << std::endl;
-        createWindowsCommand( "npm create vite");
-        COMPLETED_FUNCTION();
+        SystemCommands::createWindowsCommand( "npm create vite");
+        Type::COMPLETED_FUNCTION();
 
         exit(0);
     }
